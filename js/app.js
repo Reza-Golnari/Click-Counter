3076d0b43e159ff7808556e5f0605e3515123b1a
Reza <i.am.reza.programmer@gmail.com>
2023-09-11 16:43:31 +0330

    sorry!

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ let App = new Vue({
   el: "#app",
   data() {
     return {
-      darkMood: false,
+      darkMode: false,
       time: 5,
       counter: 0,
     };
@@ -11,17 +11,17 @@ let App = new Vue({
     lightTheme() {
       this.$refs.lightIcon.classList.add("hide-box");
       this.$refs.darkIcon.classList.remove("hide-box");
-      this.darkMood = !this.darkMood;
+      this.darkMode = !this.darkMode;
     },
     darkTheme() {
       this.$refs.lightIcon.classList.remove("hide-box");
       this.$refs.darkIcon.classList.add("hide-box");
-      this.darkMood = !this.darkMood;
+      this.darkMode = !this.darkMode;
     },
   },
   watch: {
-    darkMood() {
-      if (this.darkMood) {
+    darkMode() {
+      if (this.darkMode) {
         document.querySelector(":root").classList.add("dark");
       } else {
         document.querySelector(":root").classList.remove("dark");
